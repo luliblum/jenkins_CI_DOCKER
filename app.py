476932813e711eddfcfa142c59502cef45531f7c
jonathan blum <45519651+luliblum@users.noreply.github.com>
2019-12-12 15:33:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,23 +8,23 @@ app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
  
-currentDT = datetime.datetime.now()
-print (str(currentDT))
+#currentDT = datetime.datetime.now()
+#print (str(currentDT))
 
 
-#def get_hit_count():
-#    retries = 5
-#    while True:
-#        try:
-#            return cache.incr('hits')
-#        except redis.exceptions.ConnectionError as exc:
-#            if retries == 0:
-#                raise exc
-#            retries -= 1
-#            time.sleep(0.5)
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 
-#@app.route('/')
-#def hello():
-#    count = get_hit_count()
-#    return 'Hello From Jenkins!!!!!\n'.format(count)
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello From Jenkins!!!!!\n'.format(count)
